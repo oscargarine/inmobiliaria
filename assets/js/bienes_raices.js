35fbc32c29a295ec1ef9dba0674ac2e83b8bcfd9
Oscar Garin <oscargarin@gmail.com>
2024-12-13 11:46:05 -0300

    primera carga

---
+++ assets/js/bienes_raices.js
@@ -0,0 +1,104 @@
+const propiedades_venta = [
+  {
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: 4,
+    banos: 4,
+    costo: 5000000,
+    smoke: false,
+    pets: false
+  },
+
+  {
+    nombre: 'Apartamento acogedor en la montaña',
+    src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+    descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+    ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+    habitaciones: 2,
+    banos: 1,
+    costo: 1200000,
+    smoke: true,
+    pets: true
+  },
+  
+  {
+    nombre: 'Penthouse de lujo con terraza panorámica',
+    src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+    descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+    ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+    habitaciones: 3,
+    banos: 3,
+    costo: 4500000,
+    smoke: false,
+    pets: true
+  },
+
+  {
+    nombre: 'Domo Geodesico con hermosa vista',
+    src: 'https://lonaschile.cl/wp-content/uploads/2023/04/domo_geodesico-chile-1024x767.jpg',
+    descripcion: 'Este domo no solo ofrece una linda vista, también mucha comodidad.',
+    ubicacion: 'Fundo Curaumilla 12345, Laguna Verde, Valparaíso',
+    habitaciones: 3,
+    banos: 3,
+    costo: 1300000,
+    smoke: false,
+    pets: true
+  }
+  
+
+  ]
+
+  const propiedades_alquiler = [
+    {
+      nombre: 'Apartamento en el centro de la ciudad',
+      src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+      descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+      ubicacion: '123 Main Street, Anytown, CA 91234',
+      habitaciones: 2,
+      banos: 2,
+      costo: 400000,
+      smoke: false,
+      pets: true
+    },
+  
+    {
+      nombre: 'Apartamento luminoso con vista al mar',
+      src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+      descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+      ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+      habitaciones: 3,
+      banos: 3,
+      costo: 500000,
+      smoke: true,
+      pets: true
+    },
+  
+    {
+      nombre: 'Condominio moderno en zona residencial',
+      src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+      descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencialEste hermoso apartamento ofrece una vista impresionante al mar',
+      ubicacion: '123 Main Street, Anytown, CA 91234',
+      habitaciones: 2,
+      banos: 2,
+      costo: 200000,
+      smoke: false,
+      pets: false
+    },
+
+    {
+      nombre: 'Linda cabaña en Maitencillo, Chile',
+      src: 'https://www.guiature.cl/images/fuerzas_aereas/maitencillo/CABANA%203.JPG',
+      descripcion: 'Centro recreativo ubicado en el hermoso balneario de Maitencillo',
+      ubicacion: 'Padre del Río s/n, Maitencillo, Comuna de Puchuncaví',
+      habitaciones: 3,
+      banos: 3,
+      costo: 500000,
+      smoke: false,
+      pets: true
+    }
+
+    ]
+
+    export { propiedades_venta, propiedades_alquiler }
---
+++ assets/js/propiedades_alquiler.js
@@ -0,0 +1,55 @@
+import { propiedades_venta, propiedades_alquiler} from './bienes_raices.js'
+console.log(propiedades_venta, propiedades_alquiler)
+
+
+const mostrarPropiedades = (propiedades, sectionID) => {
+  const container = document.getElementById(sectionID)
+  const propiedadesIndex = propiedades
+
+  // iteramos el array
+  for (const propiedad of propiedadesIndex){
+    const divCard = document.createElement('div')
+     divCard.className = 'col-md-4 mb-4'
+    const cardHTML = `
+            <div class="card">
+              <img
+                src="${propiedad.src}"
+                class="card-img-top"
+                alt="${propiedad.nombre}"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${propiedad.nombre}
+                </h5>
+                <p class="card-text">
+                  ${propiedad.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                  <i class="fas fa-bath"></i> ${propiedad.banos}
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo.toLocaleString('es-CL')}</p>
+
+                <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}">
+                  <i class="fas ${propiedad.smoke ? 'fa-smoking' : 'fa-smoking-ban'}"></i>
+                  ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'}
+                </p>
+                <p class="${propiedad.pets ? 'text-success' : 'text-danger'}">
+                  <i class="${propiedad.pets ? 'fa-solid fa-paw' : 'fa-solid fa-ban'}"></i>
+                  ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}
+                </p>
+              </div>
+            </div>
+
+    `
+    divCard.innerHTML = cardHTML
+    container.appendChild(divCard)
+  } 
+}
+
+//mostrarPropiedades(propiedades_venta, 'propiedadesVenta')
+mostrarPropiedades(propiedades_alquiler, 'propiedadesAlquiler')
+
